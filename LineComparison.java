8d14caddf609ac70346a38696bbe1b827807dd18
AV-NASH <avinash21213434@gmail.com>
2020-09-25 18:30:13 +0530

    Adding welcome message in master

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package lineComparsionPackage;
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to line comparison problem\n");
+		
+	}
+
+}
